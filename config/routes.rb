f4eb50f635adeeeb60d7e1d8b2da32fff415c473
francklopes <sr.sube@gmail.com>
2024-07-05 10:37:48 -0300

    Add namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :recruiters, param: :_username
+  post 'auth/login', to: 'authentication#login'
+
+  namespace :recruiter do
+    resources :jobs
+    resources :submissions
+  end
+
+  namespace :publica do
+    resources :jobs, only: [:index, :show]
+    resources :submissions, only: :create
+  end
 end
